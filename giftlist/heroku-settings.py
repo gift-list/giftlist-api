8aa17f4c99a139031ae6c0a92acb5043ccb24802
Jeff <jeff.newburn@theironyard.com>
2016-01-06 10:31:39 -0800

    Adding secret key variable

--- giftlist/heroku-settings.py
+++ giftlist/heroku-settings.py
@@ -6,6 +6,8 @@ DATABASES['default'] =  dj_database_url.config()
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
