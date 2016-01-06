3810559a491dcbcd64a4cbb0dddc46324348de02
Jeff <jeff.newburn@theironyard.com>
2016-01-06 13:28:26 -0800

    Adding tests and pulling some coverage requirements

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,46 @@
+from api.permissions import IsOwnerOrReadOnly
+from django.contrib.auth.models import User
 from django.test import TestCase
+from lists.models import EventList
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase, APIRequestFactory, \
+    force_authenticate
 
-# Create your tests here.
+
+class TestPermissions(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user('test', 'test@test.com',
+                                             'jiofdajiofas')
+        self.user2 = User.objects.create_user('test2', 'test2@test.com',
+                                     'jiofdajiofas')
+
+        self.list = EventList.objects.create(name='Christmas', owner=self.user,
+                                        active=True)
+
+    def test_isownerreadonly_safe(self):
+
+        factory = APIRequestFactory()
+        request = factory.get(reverse('eventlist-listcreate'))
+
+        perm = IsOwnerOrReadOnly()
+        self.assertTrue(perm.has_object_permission(request,None, self.list))
+
+
+    def test_isownerreadonly_owner(self):
+
+        factory = APIRequestFactory()
+        request = factory.post(reverse('eventlist-listcreate'))
+        request.user = self.user
+
+        perm = IsOwnerOrReadOnly()
+        self.assertTrue(perm.has_object_permission(request, None, self.list))
+
+    def test_isownerreadonly_non_owner(self):
+
+        factory = APIRequestFactory()
+        request = factory.post(reverse('eventlist-listcreate'))
+        request.user = self.user2
+
+        perm = IsOwnerOrReadOnly()
+        self.assertFalse(perm.has_object_permission(request, None, self.list))
--- lists/urls.py
+++ lists/urls.py
@@ -6,7 +6,7 @@ from rest_framework.authtoken import views
 
 urlpatterns = [
     url(r'^event-lists/(?P<pk>\d+)/', DetailUpdateEventList.as_view()),
-    url(r'^event-lists/', ListCreateEventList.as_view()),
+    url(r'^event-lists/', ListCreateEventList.as_view(), name='eventlist-listcreate'),
     url(r'^items/(?P<pk>\d+)/', DetailUpdateDestroyItem.as_view()),
     url(r'^items/', ListCreateItem.as_view()),
     url(r'^pledge/(?P<pk>\d+)/', DetailUpdateDestroyPledge.as_view()),
