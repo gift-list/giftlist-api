bc7b74ec8e45175b10094341eb24ceb9df9f08f2
Jeff <jeff.newburn@theironyard.com>
2016-01-07 09:54:39 -0800

    Adding admin for user app

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from users.models import UserProfile, Address
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "created_at")
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ("owner", "street", "city", "state", "zip")
