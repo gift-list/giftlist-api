887e11edc841c1d833cee8fbdf13551c7b32b415
Jeff <jeff@goproject100.com>
2016-04-12 16:45:14 -0700

    Adding logging

--- giftlist/settings.py
+++ giftlist/settings.py
@@ -145,3 +145,21 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     )
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
